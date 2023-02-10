2f36289df78544858027f0c8533e6e91ba5ea2b2
VovkYaroslav <vovkyarik32@gmail.com>
2023-02-10 20:35:01 +0200

    hw_2.1

--- src/api/MissionsAPI.js
+++ src/api/MissionsAPI.js
@@ -1,3 +1,5 @@
 import axios from "axios";
 
-const axiosAPI = axios.create({baseUrl('')})
+const axiosAPI = axios.create({baseURL: 'https://api.spacexdata.com'});
+
+export {axiosAPI};
---
+++ src/api/servicesMissions.js
@@ -0,0 +1,5 @@
+const servicesMissions = {
+
+    getAll:
+
+}
